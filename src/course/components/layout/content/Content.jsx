4494b403ccb38fc2144b59c777b12b19e9d0f9b3
emammad22 <mammadoveltun22@gmail.com>
2024-07-16 12:14:48 +0400

    added new signature fn

--- src/course/components/layout/content/Content.jsx
+++ src/course/components/layout/content/Content.jsx
@@ -1,111 +1,138 @@
-import React from 'react'
-import Logo from '/logo/logo.svg'
-import EtibarM from '/signatures/EtibarM.svg'
-import Malakkhanim from '/signatures/Melekxanim.svg'
-import Cavid from '/signatures/Cavid.svg'
-import { useMainContext } from '../../../../context/context'
-import { useState } from 'react'
-import { useEffect } from 'react'
-
+import React, { useContext } from "react";
+import Logo from "/logo/logo.svg";
+import EtibarM from "/signatures/EtibarM.svg";
+import Alex from '../../../../../public/signatures/Alex2.png'
+import Aynur from '../../../../../public/signatures/Aynur4.png'
+import Cavid from "/signatures/Cavid.svg";
+import { MainContext, useMainContext } from "../../../../context/context";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Content = () => {
   const contentData = {
-    'Data Analyst': {
-      'title': "Successfully completed all requirements of the studies and exercises for 190 hours in the intensive Data Science Bootcamp and graduated as Certified Data Analyst and has earned the distinction and credential of Data Science Academy",
-      'content': "This specialization provided the learner with knowledge of data cleaning, visualization, machine learning and programming using R, Python, Tableau, SPSS, SPSS Modeler, SQL, Hadoop, Spark, Spark SQL, Spark MLlib, Pytorch, AWS, Mongo DB, MapReduce, Hive and Tensorflow tools simultaneously. The candidate has successfully completed the Data Science Bootcamp program and received good grades, is capable of efficiently using the tools for  visualization and data analyzing including Tableau, SPSS, SQL and has strong professional skills. The candidate is capable of inspecting, cleansing, describing, transforming and modeling data with the goal of discovering useful information."
+    "Data Analyst": {
+      title:
+        "Successfully completed all requirements of the studies and exercises for 190 hours in the intensive Data Science Bootcamp and graduated as Certified Data Analyst and has earned the distinction and credential of Data Science Academy",
+      content:
+        "This specialization provided the learner with knowledge of data cleaning, visualization, machine learning and programming using R, Python, Tableau, SPSS, SPSS Modeler, SQL, Hadoop, Spark, Spark SQL, Spark MLlib, Pytorch, AWS, Mongo DB, MapReduce, Hive and Tensorflow tools simultaneously. The candidate has successfully completed the Data Science Bootcamp program and received good grades, is capable of efficiently using the tools for  visualization and data analyzing including Tableau, SPSS, SQL and has strong professional skills. The candidate is capable of inspecting, cleansing, describing, transforming and modeling data with the goal of discovering useful information.",
     },
-    'Participant': {
-      'title': "Proficiently completed all requirements of the studies and exercises for 190 hours in the intensive Data Science Bootcamp and graduated as Participant and has earned the credential of Data Science Academy",
-      'content': "This specialization provided the learner with knowledge of data cleaning, visualization, machine learning and programming using R, Python, Tableau,SPSS, SPSS Modeler, SQL, Hadoop, Spark, Spark SQL, Spark MLlib, Pytorch, AWS, Mongo DB, MapReduce, Hive and Tensorflow tools simultaneously. The person has completed the Data Science Bootcamp wit"
-
+    Participant: {
+      title:
+        "Proficiently completed all requirements of the studies and exercises for 190 hours in the intensive Data Science Bootcamp and graduated as Participant and has earned the credential of Data Science Academy",
+      content:
+        "This specialization provided the learner with knowledge of data cleaning, visualization, machine learning and programming using R, Python, Tableau,SPSS, SPSS Modeler, SQL, Hadoop, Spark, Spark SQL, Spark MLlib, Pytorch, AWS, Mongo DB, MapReduce, Hive and Tensorflow tools simultaneously. The person has completed the Data Science Bootcamp wit",
     },
-    'Data Scientist': {
-      'title': "Proficiently completed all requirements of the studies and exercises for 190 hours in the intensive Data Science Bootcamp and graduated as Certified Data Scientist and has earned the distinction and credential of Data Science Academy",
-      'content': "This specialization provided the learner with knowledge of data cleaning, visualization, machine learning and programming using R, Python, Tableau, SPSS, SPSS Modeler, SQL, Hadoop, Spark, Spark SQL, Spark MLlib, Pytorch, AWS, Mongo DB, MapReduce, Hive and Tensorflow tools simultaneously. The  candidate is a data expert who proficiently completed the Data Science Bootcamp and achieved exceptional results with technical skills and ability to  solve complex business problems, create a variety of scalable machine learning models for company needs and tracking of its performance. The Data Scientist is well-suited to work in a team, possessing a passion for data science and data analysis, machine learning and creating specific systems and tracking how they perform over time with the goal of discovering useful information with coding skills"
+    "Data Scientist": {
+      title:
+        "Proficiently completed all requirements of the studies and exercises for 190 hours in the intensive Data Science Bootcamp and graduated as Certified Data Scientist and has earned the distinction and credential of Data Science Academy",
+      content:
+        "This specialization provided the learner with knowledge of data cleaning, visualization, machine learning and programming using R, Python, Tableau, SPSS, SPSS Modeler, SQL, Hadoop, Spark, Spark SQL, Spark MLlib, Pytorch, AWS, Mongo DB, MapReduce, Hive and Tensorflow tools simultaneously. The  candidate is a data expert who proficiently completed the Data Science Bootcamp and achieved exceptional results with technical skills and ability to  solve complex business problems, create a variety of scalable machine learning models for company needs and tracking of its performance. The Data Scientist is well-suited to work in a team, possessing a passion for data science and data analysis, machine learning and creating specific systems and tracking how they perform over time with the goal of discovering useful information with coding skills",
     },
-    'Junior Data Scientist': {
-      'title': "Proficiently completed all requirements of the studies and exercises for 190 hours in the intensive Data  Science Bootcamp and graduated as Certified Junior Data Scientist and has earned the distinction and credential of Data Science Academy",
-      'content': "This specialization provided the learner with knowledge of data cleaning, visualization, machine learning and programming using R, Python, Tableau, SPSS, SPSS Modeler, SQL, Hadoop, Spark, Spark SQL, Spark MLlib, Pytorch, AWS, Mongo DB, MapReduce, Hive and Tensorflow tools simultaneously. The participant successfully completed the Data Science Bootcamp with excellent grades and has the necessary programming, analytical, technical, research, business, and software skills such as SPSS, SQL, visualization tools, and to some extent R and Python. A Junior Data Scientist is able to work in a team, possessing a passion for data science and data analysis, machine learning and creating specific systems and tracking how they perform over time, is capable of inspecting, cleansing, describing, transforming and modeling data with the goal of discovering useful information with coding skills."
-    }
+    "Junior Data Scientist": {
+      title:
+        "Proficiently completed all requirements of the studies and exercises for 190 hours in the intensive Data  Science Bootcamp and graduated as Certified Junior Data Scientist and has earned the distinction and credential of Data Science Academy",
+      content:
+        "This specialization provided the learner with knowledge of data cleaning, visualization, machine learning and programming using R, Python, Tableau, SPSS, SPSS Modeler, SQL, Hadoop, Spark, Spark SQL, Spark MLlib, Pytorch, AWS, Mongo DB, MapReduce, Hive and Tensorflow tools simultaneously. The participant successfully completed the Data Science Bootcamp with excellent grades and has the necessary programming, analytical, technical, research, business, and software skills such as SPSS, SQL, visualization tools, and to some extent R and Python. A Junior Data Scientist is able to work in a team, possessing a passion for data science and data analysis, machine learning and creating specific systems and tracking how they perform over time, is capable of inspecting, cleansing, describing, transforming and modeling data with the goal of discovering useful information with coding skills.",
+    },
   };
   const { selectedValue } = useMainContext();
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const { signature } = useContext(MainContext);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   useEffect(() => {
     const { title: newTitle, content: newContent } = contentData[selectedValue];
     setTitle(newTitle);
     // TEST
-    
+
     setContent(newContent);
   }, [selectedValue]);
 
-
   return (
-    <div className='print:mx-7 '>
-      <div className='px-16 font-montserrat'>
-        <textarea rows='1' className='w-full focus:outline-none capitalize pb-2 -tracking-wide  my-2 font-light text-5xl'>Xəyal xudiyev</textarea>
-        <textarea onChange={(e) => setTitle(e.target.value)} value={title} rows="4" className='title w-full focus:outline-none text-[17px] text-justify leading-[22px] font-medium tracking-[-0.018em] '>
+    <div className="print:mx-7 ">
+      <div className="px-16 font-montserrat">
+        <textarea
+          rows="1"
+          className="w-full focus:outline-none capitalize pb-2 -tracking-wide  my-2 font-normal text-5xl">
+          Xəyal xudiyev
+        </textarea>
+        <textarea
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          rows="4"
+          className="title w-full focus:outline-none text-[17px] text-justify leading-[22px] font-medium tracking-[-0.018em] ">
           {title}
         </textarea>
-        <textarea rows="1" className='w-full focus:outline-none text-2xl pb-2 pt-2  text-[#e31f60] font-bold tracking-tight'>Data Science Bootcamp</textarea>
-        <textarea onChange={(e) => setContent(e.target.value)} value={content} rows="6" className=' content w-full focus:outline-none text-[14px] print:tracking-[-0.070em]  text-justify leading-[19px] font-medium my-1'>
+        <textarea
+          rows="1"
+          className="w-full focus:outline-none text-2xl pb-2 pt-2  text-[#e31f60] font-bold tracking-tight">
+          Data Science Bootcamp
+        </textarea>
+        <textarea
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+          rows="6"
+          className=" content w-full focus:outline-none text-[14px] print:tracking-[-0.070em]  text-justify leading-[19px] font-medium my-1">
           {content}
         </textarea>
       </div>
 
-      <div className='flex-col px-10'>
-        <div className='flex justify-between items-end mt-2'>
+      <div className="flex-col px-10">
+        <div className="flex justify-between items-end mt-2">
+          <div className="flex  text-start px-8 gap-9">
+            <div className="pt-2 relative">
+              <img src={EtibarM} alt="" className="absolute bottom-6 m-2" />
 
-          <div className='flex  text-start px-8 gap-9'>
-            <div className='pt-2 relative'>
-              <img src={EtibarM} alt="" className='absolute bottom-6 m-2' />
-
-              <div className=' border-t-[1px] border-black'>
-                <p className='text-sm font-semibold font-montserrat'>Etibar Huseynli</p>
-                <p className='text-xs  leading-3'>Chief Data Scientist</p>
+              <div className=" border-t-[1px] border-black">
+                <p className="text-sm font-semibold font-montserrat">Etibar Huseynli</p>
+                <p className="text-xs  leading-3">Chief Data Scientist</p>
               </div>
             </div>
-            <div className='pt-2 relative'>
-              <img src={Malakkhanim} alt="" className='absolute bottom-7 left-10  w-24' />
-              <div className=' border-t-[1px] border-black'>
-                <p className=' text-sm font-semibold font-montserrat'>Malakkhanim Gurbanova</p>
-                <p className='text-xs leading-3'>Program Manager</p>
+            <div className="pt-2 relative">
+              <img src={signature === 'Aynur' ? Aynur : signature === 'Alex' ? Alex : null} alt="" className="absolute bottom-[2.5rem] left-[0.5rem]  w-[130px]" />
+              <div className=" border-t-[1px] border-black">
+                <p className=" text-sm font-semibold font-montserrat">
+                  {signature === "Aynur" ? "Aynur Sadigova" : signature === "Alex" ? "Alex Jafarov" : ""}
+                </p>
+                <p className="text-xs leading-3">Program Manager</p>
               </div>
-
             </div>
-            <div className='pt-2 relative'>
-              <img src={Cavid} alt="" className='absolute bottom-11 block w-svw ' />
-              <div className=' border-t-[1px] border-black'>
-                <p className='text-sm font-semibold font-montserrat'>Javid Mammadov</p>
-                <p className='text-xs  leading-3'>Career Advisor</p>
+            <div className="pt-2 relative">
+              <img src={Cavid} alt="" className="absolute bottom-11 block w-svw " />
+              <div className=" border-t-[1px] border-black">
+                <p className="text-sm font-semibold font-montserrat">Javid Mammadov</p>
+                <p className="text-xs  leading-3">Career Advisor</p>
               </div>
             </div>
           </div>
 
           <div>
-            <img src={Logo} alt="" className='w-72 mr-5 ' />
+            <img src={Logo} alt="" className="w-72 mr-5 " />
           </div>
         </div>
-        <div className='flex justify-between items-center mt-6 text-xs pl-8 '>
-          <p className='text-[#959699] font-semibold text-xs'>The validity of certificate can be verified online at : <a href="http://www.dsa.az/verification">www.dsa.az/verification</a></p>
-          <div className='flex gap-0'>
-
-            <div className=' flex tracking-[4px] flex-1 items-center text-xs'>
+        <div className="flex justify-between items-center mt-6 text-xs pl-8 ">
+          <p className="text-[#959699] font-semibold text-xs">
+            The validity of certificate can be verified online at :{" "}
+            <a href="http://www.dsa.az/verification">www.dsa.az/verification</a>
+          </p>
+          <div className="flex gap-0">
+            <div className=" flex tracking-[4px] flex-1 items-center text-xs">
               Certificate ID:
-              <textarea className='tracking-[4px] focus:outline-none text-xs print:text-xs w-32 mr-8' rows='1'>SQL-8948780
+              <textarea className="tracking-[4px] focus:outline-none text-xs print:text-xs w-32 mr-8" rows="1">
+                SQL-8948780
               </textarea>
             </div>
-            <div >
+            <div>
               {/* adsasadasdasdasd */}
-              <textarea rows="1" className=' w-44 tracking-[4px] focus:outline-none mt-1'>September 30 2023</textarea>
+              <textarea rows="1" className=" w-44 tracking-[4px] focus:outline-none mt-1">
+                September 30 2023
+              </textarea>
             </div>
           </div>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
--- src/pdfDownload/PdfDownload.jsx
+++ src/pdfDownload/PdfDownload.jsx
@@ -1,16 +1,16 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { useReactToPrint } from "react-to-print";
-import { useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import Course from "../course/Course";
 import Normal from "../intern/normal/Normal";
 import Honors from "../intern/honors/Honors";
-
-
-
+import { MainContext, MainProvider } from "../context/context";
 
 export default function PdfDownload() {
   const certificateRef = useRef();
-  const downloadPdf = useReactToPrint({ content: () => certificateRef.current, });
+  const { setSignature } = useContext(MainContext);
+  const [open, setOpen] = useState(false);
+  const downloadPdf = useReactToPrint({ content: () => certificateRef.current });
   return (
     <div className="">
       <div className="" ref={certificateRef}>
@@ -22,16 +22,42 @@ export default function PdfDownload() {
       </div>
 
       <div className="self-end w-full px-10 my-4">
-        <button onClick={downloadPdf} className="w-full my-3" >
-          <a href="#_" className=" relative w-96 px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group">
+        <button onClick={downloadPdf} className="w-full my-3">
+          <a
+            href="#_"
+            className=" relative w-96 px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group">
             <span className="absolute top-0  left-0 w-96 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
             <span className="absolute bottom-0 right-0 w-96 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
             <span className="absolute top-0  left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
             <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
             <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-            <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease w-96">Download</span>
+            <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease w-96">
+              Download
+            </span>
           </a>
         </button>
+        <div className="select-signature relative">
+          <div className="select-container bg-[#eaccea] h-10 px-4 py-2 rounded cursor-pointer">
+            <div onClick={() => setOpen((prev) => !prev)} className="flex items-center justify-center gap-[20px]">
+              <p>Select Signature</p>
+              <button>+</button>
+            </div>
+            {open ? (
+              <div className="signature-list absolute top-[40px] left-0 bg-slate-300 w-full">
+                <ul className="w-full">
+                  <li className="hover:bg-[#eaeaea] hover:text-blue-500 px-4 py-2" onClick={() => setSignature("Alex")}>
+                    Alex
+                  </li>
+                  <li
+                    className="hover:bg-[#eaeaea] hover:text-blue-500 px-4 py-2"
+                    onClick={() => setSignature("Aynur")}>
+                    Aynur
+                  </li>
+                </ul>
+              </div>
+            ) : null}
+          </div>
+        </div>
         <h1 className="mt-2">&copy; 2024 by Xəyal C. Xudiyev</h1>
       </div>
     </div>
